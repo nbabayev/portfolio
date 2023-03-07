869d3d27950215678acbea62cf102d704dfb69cb
babazadenihat <nihatbabazade0@gmail.com>
2023-03-07 17:08:41 +0400

    name changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">Nihat</span>
+            Hi, I'm <span className="text-[#915EFF]">Joger</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop websites, user interfaces <br className="sm:block hidden" />
